0ed410e30979c51f33b9f5891b30995b1f8fed76
ShibaevFntk <ShibaevFntk@gmail.com>
2025-05-22 11:40:23 +0300

    Update HW

--- src/Main.java
+++ src/Main.java
@@ -4,8 +4,7 @@ public class Main {
     public static void main(String[] args) {
 
         int ticketCost = 13686; //для хранения стоимости билета
-        int bonusStep = 20; //для хранения количества рублей для одной бонусной мили
-        int bonus = ticketCost / bonusStep; //рассчитываю количество бонусных миль, используя
+        int bonus = ticketCost / 20; //рассчитываю количество бонусных миль, используя
         // значения заведённых переменных, ответ сохраняю в новую переменную
 
         System.out.println(bonus); // вывожу на экран
